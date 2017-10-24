7232ea952a72c74bda82f818381d22ef45c8e72d
kelvinoshea <kelvin.oshea@hotmail.com>
2017-10-24 14:30:23 +1000

    html structure for graduates page

--- wp-content/themes/disrupt-theme/page-graduates.php
+++ wp-content/themes/disrupt-theme/page-graduates.php
@@ -1,4 +1,31 @@
 <?php require_once('includes/header.inc'); ?>
 
-  
+  <section>
+
+    <div class="container">
+
+      <div class="filter">
+
+      </div>
+
+      <div class="graduates">
+
+        <div class="graduates-group">
+
+          <div class="row">
+
+            <div class="graduates-single">
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </section>
+
 <?php require_once('includes/footer.inc'); ?>
