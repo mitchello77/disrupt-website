7b57da14350d61e369c74bb09033fd9de608d1d2
Zach <z.mctague@gmail.com>
2017-10-24 17:30:43 +1000

    component styles

--- wp-content/themes/disrupt-theme/single-graduate.php
+++ wp-content/themes/disrupt-theme/single-graduate.php
@@ -1,9 +1,25 @@
 <?php require_once('includes/header.inc'); ?>
 
-<section>
+<section class="graduate-single">
   <div class="container">
-    <h1>asd</h1>
-  </div>
-</section>
+
+    <div class="wrapper">
+      <div class="profile circle small"></div>
+
+      <div class="slider circle large">
+        <div class="slide"></div>
+        <div class="slide"></div>
+        <div class="slide"></div>
+      </div> <!-- .slider -->
+
+      <div class="text left-align">
+        <span class="caption">Interaction Designer</span>
+        <h2>Zachary McTague</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </div>
+
+  </div> <!-- .container -->
+</section> <!-- .graduate-single -->
 
 <?php require_once('includes/footer.inc'); ?>
