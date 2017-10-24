aaa71ff34764c9d83dca0145b0feb526f1389de1
kelvinoshea <kelvin.oshea@hotmail.com>
2017-10-24 18:27:33 +1000

    mo font changes

--- wp-content/themes/disrupt-theme/page-graduates.php
+++ wp-content/themes/disrupt-theme/page-graduates.php
@@ -14,12 +14,14 @@
 
           <div class="row">
 
-            <div class="circle medium sam">
+            <div class="circle large sam">
               <div class="graduate-name">
                 <h1>Samuel Dale</h1>
                 <h2>Samuel Dale</h2>
                 <h3>Samuel Dale</h3>
                 <h4>Samuel Dale</h4>
+                <span class="title">futura</span>
+                <span class="disrupt-title">disrupt</span>
               </div>
 
               <div class="graduate-discipline">
