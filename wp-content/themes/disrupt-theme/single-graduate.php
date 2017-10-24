eff9008c601341d325f22c33c0230fc51d4f4ff6
Zach <z.mctague@gmail.com>
2017-10-24 18:14:58 +1000

    content / circle image

--- wp-content/themes/disrupt-theme/single-graduate.php
+++ wp-content/themes/disrupt-theme/single-graduate.php
@@ -4,7 +4,10 @@
   <div class="container">
 
     <div class="wrapper">
-      <div class="profile circle small"></div>
+      <div
+      class="profile circle small"
+      style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>);"
+      ></div>
 
       <div class="slider circle large">
         <div class="slide"></div>
@@ -12,12 +15,19 @@
         <div class="slide"></div>
       </div> <!-- .slider -->
 
-      <div class="text left-align">
-        <span class="caption">Interaction Designer</span>
-        <h2>Zachary McTague</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div>
-    </div>
+      <div class="text">
+        <?php
+
+          echo "
+            <span class=\"caption\">Interaction Designer</span>
+            <h2>".get_the_title()."</h2>
+            <p>".get_field('graduate_philosophy')."</p>
+            <p>".get_field('graduate_description')."</p>
+          ";
+
+         ?>
+      </div> <!-- .text -->
+    </div> <!-- .wrapper -->
 
   </div> <!-- .container -->
 </section> <!-- .graduate-single -->
