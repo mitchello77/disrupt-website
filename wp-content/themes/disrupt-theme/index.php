2ef1442ade6a195cb8ea2486718d5a3609586ff2
Zach <z.mctague@gmail.com>
2017-10-27 11:18:29 +1000

    layout

--- wp-content/themes/disrupt-theme/index.php
+++ wp-content/themes/disrupt-theme/index.php
@@ -1,8 +1,60 @@
 <?php require_once('includes/header.inc'); ?>
 
-  <section>
-    <div class="container">
-    </div>
-  </section>
+  <main>
+    <section class="landing">
+      <div class="container">
+        <div class="title-wrapper">
+          <span>DI</span>
+          <span>SRU</span>
+          <span>PT</span>
 
+          <div class="orb"></div>
+        </div> <!-- .title-wrapper -->
+      </div> <!-- .container -->
+    </section> <!-- .landing -->
+
+    <section class="exhibition-info">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <div class="image" style="background-image: url('<?php echo THEME_URL ?>img/image-exhibition.jpg')"></div>
+          </div> <!-- .column -->
+
+          <div class="column">
+            <div class="text">
+              <span class="caption">Interactive and Visual Design</span>
+              <h2>Disrupt Exhibition</h2>
+              <p>Thriving on the thrill and excitement of the unknown, Kim's creations are a product of countless experimentations, where the 'ending' will always remain a mystery to even.</p>
+            </div>
+          </div> <!-- .column -->
+        </div> <!-- .columns -->
+      </div> <!-- .container -->
+    </section> <!-- .exhibition-info -->
+
+    <section class="sponsors">
+      <div class="container">
+        <div class="content">
+          <div class="text">
+            <span class="caption">Thank you to our</span>
+            <h2>Sponsors</h2>
+            <p>Thriving on the thrill and excitement of the unknown, Kim's creations are a product of countless experimentations, where the 'ending' will always remain a mystery to even.</p>
+            <img src="https://vignette.wikia.nocookie.net/gtawiki/images/9/9a/PlayStation_1_Logo.png/revision/latest?cb=20100130082645" alt="">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/4/41/SEGA_logo.png" alt="">
+            <img src="https://vignette2.wikia.nocookie.net/logopedia/images/3/3c/Nintendo_logo.png/revision/latest?cb=20121219042620" alt="">
+          </div>
+        </div>
+      </div> <!-- .container -->
+    </section> <!-- .sponsors -->
+
+    <section class="graduates-cta">
+      <div class="container">
+        <div class="content">
+          <div class="text">
+            <p>Thriving on the thrill and excitement of the unknown, Kim's creations are a product of.Thriving on the thrill and excitement of the unknown.</p>
+            <a class="button" href="javascript:;">Our Graduates</a>
+          </div>
+        </div>
+      </div> <!-- .container -->
+    </section> <!-- .graduates-cta -->
+  </main>
 <?php require_once('includes/footer.inc'); ?>
